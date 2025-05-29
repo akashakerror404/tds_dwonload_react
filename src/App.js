bf5895cd55c1a5748dbed997a5ee4103004a5a26
akash <akashak1602@gmail.com>
2025-05-30 00:48:15 +0530

    added analatics

--- src/App.js
+++ src/App.js
@@ -3,15 +3,21 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import BulkPdfUploader from './pages/BulkPdfUploader';
 import PanPdfDownloader from './pages/PanPdfDownloader';
-
+import { Analytics } from "@vercel/analytics/react"
 function App() {
   return (
-    <Router>
+    <>
+        <Router>
       <Routes>
         <Route path="/dinoop/*879*/547125/!/" element={<BulkPdfUploader />} />
         <Route path="/" element={<PanPdfDownloader />} />
       </Routes>
+
     </Router>
+         <Analytics />
+    
+    </>
+
   );
 }
 
